e5de0aba8f5794f8bc55eac0ca6ca7af1a1cfb86
DVSS AJAY <dvssajay@gmail.com>
2021-09-21 19:35:02 +0530

    Add files via upload

---
+++ Cityscapes Baseline Experiments/DeeplabV3-Densenet161/DD161-3/Deeplabv3-DenseNet161-Quarter.py
@@ -0,0 +1,515 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+import torch.utils.model_zoo as model_zoo
+import numpy as np
+import sys, time, os, warnings 
+from skimage.segmentation import mark_boundaries
+import matplotlib.pylab as plt
+#%matplotlib inline
+from sklearn.model_selection import train_test_split
+from torchvision.datasets import Cityscapes
+
+from torchvision.transforms.functional import to_tensor, to_pil_image
+from PIL import Image
+from albumentations import (Compose,Resize,Normalize)
+
+#mean = [0.28689554, 0.32513303, 0.28389177]
+#std = [0.18696375, 0.19017339, 0.18720214]
+mean = [0.485, 0.456, 0.406] 
+std = [0.229, 0.224, 0.225]
+h,w=512,1024
+
+transform_train = Compose([ Resize(h,w), 
+                Normalize(mean=mean,std=std)])
+
+transform_val = Compose( [ Resize(h,w),
+                          Normalize(mean=mean,std=std)])
+class myCityscapes(Cityscapes):
+    def __getitem__(self, index):
+        image = Image.open(self.images[index]).convert('RGB')
+
+        targets = []
+        for i, t in enumerate(self.target_type):
+            if t == 'polygon':
+                target = self._load_json(self.targets[index][i])
+            else:
+                target = Image.open(self.targets[index][i])
+
+            targets.append(target)
+
+        target = tuple(targets) if len(targets) > 1 else targets[0]
+
+        if self.transforms is not None:
+            sample= self.transforms(image=np.array(image), mask=np.array(target))
+#            sample = self.transform(**sample)
+            img = sample['image']
+            target = sample['mask'] 
+            #img, mask = self.transforms(np.array(image),np.array(target))
+            
+        img = to_tensor(img)
+        mask = torch.from_numpy(target).type(torch.long)
+
+        return img, mask
+    
+    def _get_target_suffix(self, mode, target_type):
+            
+        if target_type == 'instance':
+            return '{}_instanceIds.png'.format(mode)
+        elif target_type == 'semantic':
+            return '{}_labelTrainIds.png'.format(mode)
+        elif target_type == 'color':
+            return '{}_color.png'.format(mode)
+        else:
+            return '{}_polygons.json'.format(mode)
+        
+        
+train_ds = myCityscapes("./", split='train', mode='fine',target_type='semantic', transforms=transform_train,target_transform=None)
+           #transforms=None)
+val = myCityscapes("./", split='val', mode='fine', target_type='semantic', transforms=transform_val, target_transform=None)
+#           transforms=None)
+
+train , dump = torch.utils.data.random_split(train_ds, [743,2232], generator=torch.Generator().manual_seed(42))
+
+print(len(train))
+print(len(dump))
+print(len(val))
+
+#defining Dataloaders
+from torch.utils.data import DataLoader
+train_dl = DataLoader(train, batch_size=2, shuffle=True,drop_last=True)
+val_dl = DataLoader(val, batch_size=2, shuffle=False,drop_last=True)
+
+
+
+from collections import OrderedDict
+import re
+from torch.utils.model_zoo import load_url as load_state_dict_from_url
+
+__all__ = ['ResNet', 'resnet50', 'resnet101', 'resnet152','vovnet39', 'vovnet57','vovnet39', 'densenet161', 'densenet201' ]
+
+
+model_urls = {
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+    'densenet169': 'https://download.pytorch.org/models/densenet169-b2777c0a.pth',
+    'densenet201': 'https://download.pytorch.org/models/densenet201-c1103571.pth',
+    'densenet161': 'https://download.pytorch.org/models/densenet161-8d451a50.pth',
+}
+
+_GN = False
+
+class Conv2d(nn.Conv2d):
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True):
+        super(Conv2d, self).__init__(in_channels, out_channels, kernel_size, stride,
+                 padding, dilation, groups, bias)
+
+    def forward(self, x):
+        # return super(Conv2d, self).forward(x)
+        weight = self.weight
+        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
+                                  keepdim=True).mean(dim=3, keepdim=True)
+        weight = weight - weight_mean
+        std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
+        weight = weight / std.expand_as(weight)
+        return F.conv2d(x, weight, self.bias, self.stride,
+                        self.padding, self.dilation, self.groups)
+
+
+class ASPP(nn.Module):
+
+    def __init__(self, C, depth, num_classes, conv=nn.Conv2d, norm=nn.BatchNorm2d, momentum=0.0003, mult=1):
+        super(ASPP, self).__init__()
+        self._C = C
+        self._depth = depth
+        self._num_classes = num_classes
+
+        self.global_pooling = nn.AdaptiveAvgPool2d(1)
+        self.relu = nn.ReLU(inplace=True)
+        self.aspp1 = conv(C, depth, kernel_size=1, stride=1, bias=False)
+        self.aspp2 = conv(C, depth, kernel_size=3, stride=1,
+                               dilation=int(6*mult), padding=int(6*mult),
+                               bias=False)
+        self.aspp3 = conv(C, depth, kernel_size=3, stride=1,
+                               dilation=int(12*mult), padding=int(12*mult),
+                               bias=False)
+        self.aspp4 = conv(C, depth, kernel_size=3, stride=1,
+                               dilation=int(18*mult), padding=int(18*mult),
+                               bias=False)
+        self.aspp5 = conv(C, depth, kernel_size=1, stride=1, bias=False)
+        self.aspp1_bn = norm(depth, momentum)
+        self.aspp2_bn = norm(depth, momentum)
+        self.aspp3_bn = norm(depth, momentum)
+        self.aspp4_bn = norm(depth, momentum)
+        self.aspp5_bn = norm(depth, momentum)
+        self.conv2 = conv(depth * 5, depth, kernel_size=1, stride=1,
+                               bias=False)
+        self.bn2 = norm(depth, momentum)
+        self.conv3 = nn.Conv2d(depth, num_classes, kernel_size=1, stride=1)
+
+    def forward(self, x):
+        x1 = self.aspp1(x)
+        x1 = self.aspp1_bn(x1)
+        x1 = self.relu(x1)
+        x2 = self.aspp2(x)
+        x2 = self.aspp2_bn(x2)
+        x2 = self.relu(x2)
+        x3 = self.aspp3(x)
+        x3 = self.aspp3_bn(x3)
+        x3 = self.relu(x3)
+        x4 = self.aspp4(x)
+        x4 = self.aspp4_bn(x4)
+        x4 = self.relu(x4)
+        x5 = self.global_pooling(x)
+        x5 = self.aspp5(x5)
+        x5 = self.aspp5_bn(x5)
+        x5 = self.relu(x5)
+        x5 = nn.Upsample((x.shape[2], x.shape[3]), mode='bilinear',
+                         align_corners=True)(x5)
+        x = torch.cat((x1, x2, x3, x4, x5), 1)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+
+        return x
+
+    
+
+
+class _DenseLayer(nn.Sequential):
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate, dilation=1):
+        super(_DenseLayer, self).__init__()
+        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size *
+                                           growth_rate, kernel_size=1, stride=1,
+                                           bias=False)),
+        self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
+        self.add_module('relu2', nn.ReLU(inplace=True)),
+        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+                                           kernel_size=3, stride=1, padding=dilation,
+                                           dilation=dilation, bias=False)),
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        new_features = super(_DenseLayer, self).forward(x)
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate,
+                                     training=self.training)
+        return torch.cat([x, new_features], 1)
+
+
+class _DenseBlock(nn.Sequential):
+    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate, dilation):
+        super(_DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(num_input_features + i * growth_rate, growth_rate,
+                                bn_size, drop_rate, dilation)
+            self.add_module('denselayer%d' % (i + 1), layer)
+
+
+class _Transition(nn.Sequential):
+    def __init__(self, num_input_features, num_output_features, stride=2):
+        super(_Transition, self).__init__()
+        self.add_module('norm', nn.BatchNorm2d(num_input_features))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
+                                          kernel_size=1, stride=1, bias=False))
+        if stride == 2:
+            self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+
+
+class DenseNet(nn.Module):
+    r"""Densenet-BC model class, based on
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (int) - multiplicative factor for number of bottle neck layers
+          (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+    """
+    def __init__(self, 
+    			 growth_rate=32, 
+                 block_config=(6, 12, 24, 16),
+                 num_init_features=64, 
+                 bn_size=4, 
+                 drop_rate=0, 
+                 stride_list=None,
+                 dilation_list=[], 
+                 num_classes=20, 
+                 num_groups=None, 
+                 weight_std=False, 
+                 beta=False):
+
+        super(DenseNet, self).__init__()
+
+        # First convolution
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
+                                padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+        ]))
+
+        # Each denseblock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
+                                bn_size=bn_size, growth_rate=growth_rate,
+                                # drop_rate=drop_rate)
+                                drop_rate=drop_rate, dilation=dilation_list[i]) #ywlee
+            self.features.add_module('denseblock%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = _Transition(num_input_features=num_features,
+                                    num_output_features=num_features // 2,
+                                    stride=stride_list[i])
+                self.features.add_module('transition%d' % (i + 1), trans)
+                num_features = num_features // 2
+
+        # Final batch norm
+        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+
+        # ASPP layer
+        self.aspp = ASPP(num_features, 256, num_classes)
+
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.GroupNorm):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+
+    def forward(self, x):
+        size = (x.shape[2], x.shape[3])
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        x = self.aspp(out)
+        return nn.Upsample(size, mode='bilinear', align_corners=True)(x)
+
+
+def _load_state_dict(model, model_url, progress=True):
+    # '.'s are no longer allowed in module names, but previous _DenseLayer
+    # has keys 'norm.1', 'relu.1', 'conv.1', 'norm.2', 'relu.2', 'conv.2'.
+    # They are also in the checkpoints in model_urls. This pattern is used
+    # to find such keys.
+    pattern = re.compile(
+        r'^(.*denselayer\d+\.(?:norm|relu|conv))\.((?:[12])\.(?:weight|bias|running_mean|running_var))$')
+
+    model_dict = model.state_dict()
+    state_dict = load_state_dict_from_url(model_url, progress=progress)
+    for key in list(state_dict.keys()):
+        res = pattern.match(key)
+        if res:
+            new_key = res.group(1) + res.group(2)
+            state_dict[new_key] = state_dict[key]
+            del state_dict[key]
+        if 'classifier' in key:
+            del state_dict[key]
+    model_dict.update(state_dict)
+    model.load_state_dict(model_dict)
+
+
+def _densenet(arch, growth_rate, block_config, num_init_features, stride_list, dilation_list, pretrained, **kwargs):
+    
+    model = DenseNet(growth_rate=growth_rate, 
+    				 block_config=block_config, 
+    				 num_init_features=num_init_features, 
+    				 stride_list=stride_list, 
+    				 dilation_list=dilation_list, 
+    				 **kwargs)
+    if pretrained:
+        _load_state_dict(model, model_urls[arch])
+    return model
+
+
+def densenet201(pretrained=False, **kwargs):
+    r"""Densenet-201 model from
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    return _densenet(arch='densenet201', 
+                     growth_rate=32, 
+                     block_config=(6, 12, 48, 32), 
+                     num_init_features=64,
+                     stride_list=[2,2,1],
+                     dilation_list=[1,1,1,2], 
+                     pretrained=pretrained, 
+                     **kwargs)
+
+def densenet161(pretrained=False, **kwargs):
+    r"""Densenet-161 model from
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _densenet(arch='densenet161', 
+                     growth_rate=48, 
+                     block_config=(6, 12, 36, 24), 
+                     num_init_features=96,
+                     stride_list=[2,2,1],
+                     dilation_list=[1,1,1,2], 
+                     pretrained=pretrained, 
+                     **kwargs)
+
+model = densenet161(pretrained=True)
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model=model.to(device)
+
+#criterion = nn.BCEWithLogitsLoss(reduction="sum")
+criterion = nn.CrossEntropyLoss(reduction="sum")
+from torch import optim
+opt = optim.SGD(model.parameters(), lr=1e-6, momentum=0.9,nesterov=True)
+
+def loss_batch(loss_func, output, target, opt=None):   
+    loss = loss_func(output, target)
+    
+    if opt is not None:
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+    return loss.item(), None
+
+
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+lr_scheduler = ReduceLROnPlateau(opt, mode='min',factor=0.5, patience=20,verbose=1)
+
+def get_lr(opt):
+    for param_group in opt.param_groups:
+        return param_group['lr']
+
+current_lr=get_lr(opt)
+print('current lr={}'.format(current_lr))
+
+
+def loss_epoch(model,loss_func,dataset_dl,sanity_check=False,opt=None):
+    running_loss=0.0
+    len_data=len(dataset_dl.dataset)
+    for xb, yb in dataset_dl:
+        xb=xb.to(device)
+        yb=yb.to(device)
+        output=model(xb)
+        loss_b, metric_b = loss_batch(loss_func, output, yb, opt)
+        running_loss += loss_b
+        if sanity_check is True:
+            break
+    loss=running_loss/float(len_data)
+    return loss, None
+
+import copy
+def train_val(model, params):
+    num_epochs=params["num_epochs"]
+    loss_func=params["loss_func"]
+    opt=params["optimizer"]
+    train_dl=params["train_dl"]
+    val_dl=params["val_dl"]
+    sanity_check=params["sanity_check"]
+    lr_scheduler=params["lr_scheduler"]
+    path2weights=params["path2weights"]
+
+    loss_history={
+        "train": [],
+        "val": []}
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss=float('inf')
+    o = open('./DeeplabDenseNet161-Quarter.txt','w')
+    for epoch in range(num_epochs):
+        current_lr=get_lr(opt)
+
+        print('Epoch {}/{}, current lr={}'.format(epoch, num_epochs - 1, current_lr), file=o)
+
+        
+        print('Epoch {}/{}, current lr={}'.format(epoch, num_epochs - 1, current_lr))   
+
+        model.train()
+        train_loss, _ = loss_epoch(model,loss_func,train_dl,sanity_check,opt)
+        loss_history["train"].append(train_loss)
+        
+        model.eval()
+        with torch.no_grad():
+            val_loss, _ = loss_epoch(model,loss_func,val_dl,sanity_check)
+        loss_history["val"].append(val_loss)
+
+        if val_loss < best_loss:
+            best_loss = val_loss
+            best_model_wts = copy.deepcopy(model.state_dict())
+            torch.save(model.state_dict(), path2weights)
+            print("Copied best model weights!")
+            print("Copied best model weights!",file=o)
+            
+        lr_scheduler.step(val_loss)
+        if current_lr != get_lr(opt):
+            print("Loading best model weights!")
+            print("Loading best model weights!",file=o)
+            model.load_state_dict(best_model_wts)
+
+        print("train loss: %.6f" %(train_loss))
+        print("train loss: %.6f" %(train_loss),file=o)
+        print("val loss: %.6f" %(val_loss))
+        print("val loss: %.6f" %(val_loss),file=o)
+        print("-"*10)
+        print("-"*10,file=o) 
+    model.load_state_dict(best_model_wts)
+    o.close()
+    return model, loss_history
+
+
+
+start = time.time()
+
+import os
+path2models= "./DeeplabDenseNet161_Quarter"
+if not os.path.exists(path2models):
+        os.mkdir(path2models)
+params_train={
+    "num_epochs": 180,
+    "optimizer": opt,
+    "loss_func": criterion,
+    "train_dl": train_dl,
+    "val_dl": val_dl,
+    "sanity_check": False,
+    "lr_scheduler": lr_scheduler,
+    "path2weights": path2models+"weights.pt",}
+model,loss_hist=train_val(model,params_train)
+
+end = time.time()
+o = open('./DeeplabDenseNet161_Quarter_time.txt','w')
+
+print("TIME TOOK {:3.2f}MIN".format((end - start )/60), file=o)
+
+o.close()
+
+print("TIME TOOK {:3.2f}MIN".format((end - start )/60))
+
+torch.save(model.state_dict(), "DeeplabDenseNet161_Quarter_M")
+
+num_epochs=params_train["num_epochs"]
+plt.figure(figsize=(30,30))
+plt.title("Train-Val Loss")
+plt.plot(range(1,num_epochs+1),loss_hist["train"],label="train")
+plt.plot(range(1,num_epochs+1),loss_hist["val"],label="val")
+plt.ylabel("Loss")
+plt.xlabel("Training Epochs")
+plt.legend()
+plt.savefig('DeeplabDenseNet161_Quarter.png', dpi = 300)
+
+
